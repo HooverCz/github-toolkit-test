1bbb69387ca2176ffd8ff440206bd7963d54a4c2
Simon DALECKY <>
2023-08-03 14:08:06 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='github_toolkit',
+    version='0.0.1',
+    packages=[
+        'gso_github_toolkit',
+    ],
+    license='',
+    author='Simon Dalecky',
+    description='package to update organisational secret with Github app private key',
+    python_requires='>=3.6',
+    install_requires=['requests==2.25.1', 'octokitpy==0.15.0', 'pynacl==1.5.0']
+)
